cbf32951a61d47762944e0396099809c041bb3d7
garrik <garrik@mail.ru>
2024-11-05 12:14:48 +0300

    remove code with publish feature

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -2,60 +2,35 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.compose.compiler)
-    `maven-publish`
 }
 
 android {
-    namespace = "com.mrozon.common"
     compileSdk = 34
+    namespace = "com.mrozon.common"
 
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
-        isCoreLibraryDesugaringEnabled = true
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "11"
     }
 }
 
-dependencies {
-    coreLibraryDesugaring(libs.desugar.jdk.libs)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.logging.timber)
-    implementation(libs.androidx.material3)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui.tooling.preview)
+kotlin {
+    sourceSets {
+        java {
+            dependencies {
+                implementation(libs.androidx.lifecycle.runtime.ktx)
+                implementation(libs.logging.timber)
+                implementation(libs.androidx.material3)
+                implementation(platform(libs.androidx.compose.bom))
+                implementation(libs.androidx.ui.tooling.preview)
 
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("common") {
-            groupId = "com.github.MrOzOn"
-            artifactId = "common"
-            version = "0.1.6"
-            from(components.findByName("src"))
+                implementation(libs.material)
+                testImplementation(libs.junit)
+            }
         }
     }
 }
+
